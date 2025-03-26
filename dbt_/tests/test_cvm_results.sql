c0dc95a93555f31a1ed2d81d8e8fd3f18edf1faa
V. Prana <11522296+20k-P@users.noreply.github.com>
2025-03-26 22:17:41 +0100

    Vinca tache178 update table cvm (#102)
    * first try
    
    * sur la bonne voie
    
    * fix: right number of commune per year in final query
    
    * fix: CASE WHEN resultat
    
    * clean
    
    * fix: nb_analyse dans table finale
    
    * Update
    
    * fix: no more duplicate
    
    * feat: WIP use new inter tables
    
    * feat: use new inter tables
    
    * feat: update ana__resultats_communes.sql
    
    * feat: update table ana_results_commnues
    
    * fix: update doc ana__resultats_communes.sql
    
    * feta: run notebook cvm_calcul_conformite_par_commune_LastPvl.ipynb
    
    * feat: intermediate table for cvm - bilan annuel
    
    * fix: precommit
    
    * fix: typo into yaml for int__resultats_annuel_cvm_communes
    
    * fix: typo into yaml
    
    * fix: update ymal
    
    * feat: all table for cvm - 6 in total
    
    * fix: test DBT
    
    * fix: test DBT
    
    * fix: test DBT
    
    * fix: test DBT
    
    * fix: fix naming to last_datetimeprel
    
    * fix: fix naming to commune_code_insee
    
    * fix: fix naming to commune_code_insee
    
    * fix: fix naming to commune_code_insee in last_cvm_communes
    
    * fix: fix naming to commune_code_insee in last_cvm_communes last JOIN
    
    * fix: DBT accepted_values
    
    * fix: je remets un test que j'avais enlevé par erreur sur une table précédent
    
    * fix: simplify and take review comment
    
    * feat: add test categorie cvm
    
    * fix: pre-commit
    
    * fix: error CASE WHEN
    
    * fix: no communes_name
    
    * fix: last_datetimeprel
    
    * feat: update notebook to check
    
    * core: precommit
    
    * fix: _int__models.yml
    
    * fix: DBT check - erreur liée a >=/<=/>/< non homogene
    
    * update documentation notebook
    
    * fix: rename uid to udi
    
    * feat: add notebook to check vlatraduite et rqana
    
    * feat: new table ana__resultats_annee_udi
    
    * feat: new table ana__resultats_last_udi
    
    * feat: test add percent_ko for int__resultats_annuel_cvm_udi
    
    * feat: reorganize files + use valdraduite_corrigée
    
    * fix: quick SQL fix after jeremy first reviexw
    
    * feat: regorganize: add all folder and ana_resultats_udi
    
    * fix: rename valtraduite to valtraduite_corrigee
    
    * fix: DBT errors
    
    * fix: remove cdparametresiseeaux from dbt_/models/analytics/_ana__models.yml'
    
    * feat: add pourcentage_depassement and switch valtraduite_corrigee  to valtraduite
    
    * fix: precommit
    
    * feat: update notebook and SQL annuel
    
    * feat: Add singular data tests DBT
    
    * fix: FROM clause into tests/check_some_values_cvm_udi
    
    * update cvm results
    
    * deactivate non reviewed models
    
    ---------
    
    Co-authored-by: Jeremy Greze <jereze@users.noreply.github.com>

---
+++ dbt_/tests/test_cvm_results.sql
@@ -0,0 +1,100 @@
+-- dernier relevé
+SELECT
+    'dernier relevé' AS periode,
+    cdreseau,
+    categorie,
+    resultat,
+    0 AS nb_depassements,
+    0 AS nb_prelevements,
+    0 AS ratio_depassements
+FROM
+    {{ ref('int__resultats_cvm_udi_dernier') }}
+WHERE
+    (
+        cdreseau = '976003489'
+        AND categorie = 'cvm'
+        AND last_datetimeprel = '2024-07-16 08:30:00'
+        AND resultat != 'non_quantifie'
+    )
+    OR
+    (
+        cdreseau = '001000241'
+        AND categorie = 'cvm'
+        AND last_datetimeprel = '2024-12-31 14:00:00'
+        AND resultat != 'non_quantifie'
+    )
+    OR
+    (
+        cdreseau = '087003637'
+        AND categorie = 'cvm'
+        AND last_datetimeprel = '2024-07-04 10:50:00'
+        AND resultat != 'sup_0_5'
+    )
+    OR
+    (
+        cdreseau = '095004048'
+        AND categorie = 'cvm'
+        AND last_datetimeprel = '2024-07-23 08:26:00'
+        AND resultat != 'inf_0_5'
+    )
+UNION ALL
+-- annuel
+SELECT
+    'annuel' AS periode,
+    cdreseau,
+    categorie,
+    '' AS resultat,
+    nb_depassements,
+    nb_prelevements,
+    ratio_depassements
+FROM
+    {{ ref('int__resultats_cvm_udi_annuel') }}
+WHERE
+    (
+        cdreseau = '001001073'
+        AND categorie = 'cvm'
+        AND annee = '2024'
+        AND nb_depassements != 1
+    )
+    OR
+    (
+        cdreseau = '001001073'
+        AND categorie = 'cvm'
+        AND annee = '2024'
+        AND ratio_depassements != 0.25
+    )
+    OR
+    (
+        cdreseau = '001001073'
+        AND categorie = 'cvm'
+        AND annee = '2023'
+        AND nb_depassements != 0
+    )
+    OR
+    (
+        cdreseau = '001001073'
+        AND categorie = 'cvm'
+        AND annee = '2022'
+        AND nb_depassements != 0
+    )
+    OR
+    (
+        cdreseau = '007000088'
+        AND categorie = 'cvm'
+        AND annee IN ('2022', '2023', '2024')
+        AND nb_depassements != 0
+    )
+    OR
+    (
+        cdreseau = '095004048'
+        AND categorie = 'cvm'
+        AND annee = '2024'
+        AND nb_prelevements != 12
+    )
+    OR
+    (
+        cdreseau = '005001358'
+        AND categorie = 'cvm'
+        AND annee = '2022'
+        AND nb_depassements != 2
+    )
